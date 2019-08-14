cac3705f2e8dd4f6cec735b5b304c33f43d9c000
Mostafa khaldi <mostafa.khaldi@outlook.com>
2019-08-14 10:14:17 +0100

    Adding Footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,25 @@
 import React from "react"
+import logo from "./images/logo.png"
 
 function Footer(){
     return(
-        <div>
-            <h1>Footer</h1>
+        <div className="footer">
+            <div className="wrapper">
+                <div className="footer-info">
+                    <img src={logo} alt="white-logo" />
+                    <div>
+                        <ul>
+                            <li>About</li>
+                            <li>Policy</li>
+                            <li>Contact</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr/>
+                <div className="copyright">
+                    <p>Copyright &copy; 2019 <strong>TravMag</strong> | All rights reserved</p>
+                </div>
+            </div>
         </div>
     )
 }
