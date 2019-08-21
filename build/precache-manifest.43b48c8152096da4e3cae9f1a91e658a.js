8bd0e1dcddef9fcbe7ec5afcd4075eb11f86398b
Mostafa khaldi <mostafa.khaldi@outlook.com>
2019-08-21 11:20:19 +0100

    Editing package.json file

---
+++ build/precache-manifest.43b48c8152096da4e3cae9f1a91e658a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d0be0f99409368d162954a077b77a9b",
+    "url": "/TravMag/index.html"
+  },
+  {
+    "revision": "dbde94c574f42c1e6dce",
+    "url": "/TravMag/static/js/2.5a0bc9af.chunk.js"
+  },
+  {
+    "revision": "db141afb38ba8be570f2",
+    "url": "/TravMag/static/js/main.b5f7d9f8.chunk.js"
+  },
+  {
+    "revision": "a9aebd0ecfe83cfe235f",
+    "url": "/TravMag/static/js/runtime~main.064b572e.js"
+  },
+  {
+    "revision": "a45e9c43306cb84c6558648b0e09445c",
+    "url": "/TravMag/static/media/spinnerGIF.a45e9c43.svg"
+  }
+]);
