711d676ea12b52734eac843c871981f712ed1910
Mostafa khaldi <mostafa.khaldi@outlook.com>
2019-08-22 14:57:46 +0100

    Updates

---
+++ precache-manifest.0f223b77fc9e34ddf67d6dcffbd96096.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "067d17b215d38b96674f403d5d8c6e13",
+    "url": "/TravMag/index.html"
+  },
+  {
+    "revision": "dbde94c574f42c1e6dce",
+    "url": "/TravMag/static/js/2.5a0bc9af.chunk.js"
+  },
+  {
+    "revision": "f2c05ff04f9d5239e39a",
+    "url": "/TravMag/static/js/main.a4c07902.chunk.js"
+  },
+  {
+    "revision": "a9aebd0ecfe83cfe235f",
+    "url": "/TravMag/static/js/runtime~main.064b572e.js"
+  },
+  {
+    "revision": "a45e9c43306cb84c6558648b0e09445c",
+    "url": "/TravMag/static/media/spinnerGIF.a45e9c43.svg"
+  }
+]);
