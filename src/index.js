4e37e30a10538e0d9c37a6ca3a31ad69ffd6a652
Mostafa khaldi <mostafa.khaldi@outlook.com>
2019-08-22 15:31:24 +0100

    fixes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDom from "react-dom"
 import App from "./App"
-import {HashRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router} from "react-router-dom"
 
-ReactDom.render(<Router><App/></Router>,document.getElementById("root"))
-// basename={process.env.PUBLIC_URL}
+ReactDom.render(<Router basename={process.env.PUBLIC_URL}><App/></Router>,document.getElementById("root"))
+
